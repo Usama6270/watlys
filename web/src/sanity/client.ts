e63afc614076cdf36c0523473d664ae91a68cd67
Usama6270 <usamaunder19@gmail.com>
2026-09-03 00:33:55 +0500

    Clean up old web and studio folders

--- web/src/sanity/client.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "next-sanity";
-
-export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "r6fj3reg",
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-  apiVersion: "2026-08-30",
-  useCdn: true,
-});
